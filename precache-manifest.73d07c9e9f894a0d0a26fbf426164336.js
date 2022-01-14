cf0da3259c2f5eaee44c54c4436e01220fcdf513
moehrle007 <moehri007@gmail.com>
2022-01-14 22:08:47 +0100

    Updates

---
+++ precache-manifest.73d07c9e9f894a0d0a26fbf426164336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f3c8abed3cd1ce7fef94683c61eb5a",
+    "url": "/Bequit-lesson/index.html"
+  },
+  {
+    "revision": "fd9552345c1313bb3156",
+    "url": "/Bequit-lesson/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/Bequit-lesson/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/Bequit-lesson/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "fd9552345c1313bb3156",
+    "url": "/Bequit-lesson/static/js/main.05f82a2b.chunk.js"
+  },
+  {
+    "revision": "f4225810223c2fb641c7",
+    "url": "/Bequit-lesson/static/js/runtime~main.42e09efb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Bequit-lesson/static/media/logo.06e73328.svg"
+  }
+]);
